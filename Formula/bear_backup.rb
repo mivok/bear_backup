ea8ed812fc7f4c3e69c5fc948d14c45d14bfc504
Mark Harrison <mark@mivok.net>
2017-08-05 16:09:27 -0400

    Add a homebrew formula

---
+++ Formula/bear_backup.rb
@@ -0,0 +1,45 @@
+class BearBackup < Formula
+  desc "Bear backup utility"
+  homepage "https://github.com/mivok/bear_backup"
+  head "git@github.com:mivok/bear_backup", :using => :git
+
+  depends_on "python3"
+
+  def install
+    bin.mkpath
+    bin.install "bear_backup.py"
+  end
+
+  plist_options :manual => "bear_backup"
+
+  def plist
+    <<-EOF.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+        "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+        <dict>
+             <key>Label</key>
+             <string>#{plist_name}</string>
+             <key>ProgramArguments</key>
+             <array>
+                 <string>#{HOMEBREW_PREFIX}/bin/bear_backup.py -n ~/Dropbox/backups/bear</string>
+             </array>
+             <key>RunAtLoad</key>
+             <false/>
+             <key>StartCalendarInterval</key>
+                 <dict>
+                 <key>Hour</key>
+                 <integer>0</integer>
+                 <key>Minute</key>
+                 <integer>0</integer>
+             </dict>
+        </dict>
+    </plist>
+    EOF
+  end
+
+  test do
+    system bin/"bear_backup.py", "--help"
+  end
+end
